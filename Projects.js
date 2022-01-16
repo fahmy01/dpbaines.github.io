b28439d420cccca30e652706c9913bff91917173
David Baines <david.peter.baines@gmail.com>
2022-01-16 15:03:41 -0500

    New revision to experiences section, updating about me

--- Projects.js
+++ Projects.js
@@ -1,12 +1,53 @@
 
-class Projects extends React.Component {
-
+class Experience extends React.Component {
     render() {
         return (
-            <div>Test</div>
+            <div className="project-container">
+                <div className="project-cell" style={{borderRightColor: this.props.hintColor}}>
+                    <div className="project-time">{this.props.time}</div>
+                    <div className="project-title">{this.props.title}</div>
+                    <div className="project-role">{this.props.role}</div>
+                    <div className="project-description">{this.props.description}</div>
+                </div>
+            </div>
         );
     }
 }
 
-//const domContainer = document.querySelector('#projects');
-//ReactDOM.render(<Projects />, domContainer);
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            exps : [{
+                "title": "Loading...",
+                "role": "",
+                "time": "",
+                "description": "",
+                "color": "#007DC3"
+            }]
+        };
+    }
+
+    componentDidMount() {
+        let expUrl = "experiences.json";
+        fetch(expUrl)
+            .then(response => response.json())
+            .then(y => this.setState({exps: y.experiences}));
+    }
+
+    render() {
+        const mappedExps = this.state.exps.map((exp) =>
+            <Experience key={exp.title} title={exp.title} hintColor={exp.color} time={exp.time} role={exp.role} description={exp.description} />
+        );  
+        // console.log(Array.isArray(mappedExps));
+
+        return (
+            <div>
+                {mappedExps}
+            </div>
+        );
+    }
+}
+
+const domContainer = document.querySelector('#projects');
+ReactDOM.render(<Projects />, domContainer);
